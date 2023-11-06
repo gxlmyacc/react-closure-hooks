8da410151d4c2bf6c16bcb9c40f7d21b8e545c9a
gxlmyacc <gxl.mymail@163.com>
2023-11-06 17:44:51 +0800

    0.0.9

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 /**
  * Suppress the warning when using useLayoutEffect with SSR. (https://reactjs.org/link/uselayouteffect-ssr)
  * Make use of useInsertionEffect if available.
